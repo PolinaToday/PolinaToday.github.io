7b564c3f0d924b59d49711679545c03f6ded78f2
PolinaToday <102701845+PolinaToday@users.noreply.github.com>
2024-07-17 15:56:58 +0300

    Update and rename pix.json to pix.js

---
+++ pix.js
@@ -0,0 +1,23 @@
+// Получаем кнопку "Купить" и модальное окно
+var buyButton = document.getElementById("buyButton");
+var modal = document.getElementById("myModal");
+
+// Получаем элемент для закрытия модального окна
+var closeButton = document.getElementsByClassName("close")[0];
+
+// Открываем модальное окно при клике на кнопку "Купить"
+buyButton.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Закрываем модальное окно при клике на кнопку закрытия
+closeButton.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Закрываем модальное окно при клике вне его области
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
