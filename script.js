854505bd8671fe67cfd8999bc5c0265b10f9444f
PolinaToday <102701845+PolinaToday@users.noreply.github.com>
2024-07-17 16:12:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-// Получаем кнопку "Купить" и модальное окно
-var buyButton = document.getElementById("buyButton");
-var modal = document.getElementById("myModal");
+document.addEventListener("DOMContentLoaded", function() {
+    // Получаем кнопку "Купить" и модальное окно
+    var buyButton = document.getElementById("buyButton");
+    var modal = document.getElementById("myModal");
 
-// Получаем элемент для закрытия модального окна
-var closeButton = document.getElementsByClassName("close")[0];
+    // Получаем элемент для закрытия модального окна
+    var closeButton = document.getElementsByClassName("close")[0];
 
-// Открываем модальное окно при клике на кнопку "Купить"
-buyButton.onclick = function() {
-    modal.style.display = "block";
-}
+    // Открываем модальное окно при клике на кнопку "Купить"
+    buyButton.onclick = function() {
+        modal.style.display = "block";
+    }
 
-// Закрываем модальное окно при клике на кнопку закрытия
-closeButton.onclick = function() {
-    modal.style.display = "none";
-}
-
-// Закрываем модальное окно при клике вне его области
-window.onclick = function(event) {
-    if (event.target == modal) {
+    // Закрываем модальное окно при клике на кнопку закрытия
+    closeButton.onclick = function() {
         modal.style.display = "none";
     }
-}
+
+    // Закрываем модальное окно при клике вне его области
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
