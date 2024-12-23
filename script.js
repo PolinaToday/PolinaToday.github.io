017a4c8e407520d5d450a25585d4b2ae7be13f43
PolinaToday <102701845+PolinaToday@users.noreply.github.com>
2024-12-23 23:58:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,128 +1,7 @@
-class Balls {
-  constructor(context, buffer) {
-    this.context = context;
-    this.buffer = buffer;
-  }
-  setup() {
-    this.gainNode = this.context.createGain();
-    this.source = this.context.createBufferSource();
-    this.source.buffer = this.buffer;
-    this.source.connect(this.gainNode);
-    this.gainNode.connect(this.context.destination);
-    this.gainNode.gain.setValueAtTime(1, this.context.currentTime);
-  }
-  play() {
-    this.setup();
-    this.source.start(this.context.currentTime);
-  }
-  stop() {
-    var ct = this.context.currentTime + 1;
-    this.gainNode.gain.exponentialRampToValueAtTime(.1, ct);
-    this.source.stop(ct);
-  }
-}
-
-class Buffer {
-  constructor(context, urls) {
-    this.context = context;
-    this.urls = urls;
-    this.buffer = [];
-  }
-  loadSound(url, index) {
-    let request = new XMLHttpRequest();
-    request.open('get', url, true);
-    request.responseType = 'arraybuffer';
-    let thisBuffer = this;
-    request.onload = function() {
-      thisBuffer.context
-        .decodeAudioData(request.response, function(buffer) {
-          thisBuffer.buffer[index] = buffer;
-          if(index == thisBuffer.urls.length-1) {
-            thisBuffer.loaded();
-          }
-        });
-    };
-    request.send();
-  };
-  getBuffer() {
-    this.urls.forEach((url, index) => {
-      this.loadSound(url, index);
-    })
-  }
-  loaded() {
-    loaded = true;
-  }
-  getSound(index) {
-    return this.buffer[index];
-  }
-}
-
-let balls = null,
-    preset = 0,
-    loaded = false;
-let path = 'audio/';
-let sounds = [
-  path + 'sound1.mp3',
-  path + 'sound2.mp3',
-  path + 'sound3.mp3',
-  path + 'sound4.mp3',
-  path + 'sound5.mp3',
-  path + 'sound6.mp3',
-  path + 'sound7.mp3',
-  path + 'sound8.mp3',
-  path + 'sound9.mp3',
-  path + 'sound10.mp3',
-  path + 'sound11.mp3',
-  path + 'sound12.mp3',
-  path + 'sound13.mp3',
-  path + 'sound14.mp3',
-  path + 'sound15.mp3',
-  path + 'sound16.mp3',
-  path + 'sound17.mp3',
-  path + 'sound18.mp3',
-  path + 'sound19.mp3',
-  path + 'sound20.mp3',
-  path + 'sound21.mp3',
-  path + 'sound22.mp3',
-  path + 'sound23.mp3',
-  path + 'sound24.mp3',
-  path + 'sound25.mp3',
-  path + 'sound26.mp3',
-  path + 'sound27.mp3',
-  path + 'sound28.mp3',
-  path + 'sound29.mp3',
-  path + 'sound30.mp3',
-  path + 'sound31.mp3',
-  path + 'sound32.mp3',
-  path + 'sound33.mp3',
-  path + 'sound34.mp3',
-  path + 'sound35.mp3',
-  path + 'sound36.mp3'
-];
-let context = new (window.AudioContext || window.webkitAudioContext)();
-
-function playBalls() {
-  let index = parseInt(this.dataset.note) + preset;
-  balls = new Balls(context, buffer.getSound(index));
-  balls.play();
-}
-
-function stopBalls() {
-  balls.stop();
-}
-
-let buffer = new Buffer(context, sounds);
-let ballsSound = buffer.getBuffer();
-let buttons = document.querySelectorAll('.b-ball_bounce');
-buttons.forEach(button => {
-  button.addEventListener('mouseenter', playBalls.bind(button));
-  button.addEventListener('mouseleave', stopBalls);
-})
-
 function ballBounce(e) {
   var i = e;
   if (e.className.indexOf(" bounce") > -1) {
-  return;
+    return;
   }
   toggleBounce(i);
 }
@@ -153,14 +32,14 @@ function toggleBounce(i){
 var array1 = document.querySelectorAll('.b-ball_bounce')
 var array2 = document.querySelectorAll('.b-ball_bounce .b-ball__right')
 
-for(var i=0; i<array1.length; i++){
-  array1[i].addEventListener('mouseenter', function(){
+for(var i = 0; i < array1.length; i++) {
+  array1[i].addEventListener('mouseenter', function() {
     ballBounce(this)
   })
 }
 
-for(var i=0; i<array2.length; i++){
-  array2[i].addEventListener('mouseenter', function(){
+for(var i = 0; i < array2.length; i++) {
+  array2[i].addEventListener('mouseenter', function() {
     ballBounce(this)
   })
 }
@@ -179,8 +58,6 @@ document.addEventListener('keydown', function (j) {
   let i = j.target;
   if (j.which in a) {
     let index = parseInt(a[j.which]);
-    balls = new Balls(context, buffer.getSound(index));
-    balls.play();
     let ball = document.querySelector('[data-note="' + index + '"]');
     toggleBounce(ball);
   }
